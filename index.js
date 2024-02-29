d8a752bacc3093357f8b70c4e04e9f77c5cd2f06
Dyonathan Cox <dyonathamcox@gmail.com>
2024-02-29 00:28:51 -0300

    update

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const segurosList = document.querySelector(".seguros ul");
 
     scrollLeftBtn.addEventListener("click", function() {
-        segurosList.scrollLeft -= 600;
+        segurosList.scrollLeft -= 260;
     });
 
     scrollRightBtn.addEventListener("click", function() {
-        segurosList.scrollLeft += 600;
+        segurosList.scrollLeft += 260;
     });
 });
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open')
+        document.querySelector('.icon').src = "assets/menu.png";
+    }else{
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/close.png";
+    }
+}
